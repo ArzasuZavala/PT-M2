c619fcef878e48391feb19928f88e8f1838ce75c
ArsazuZavala <zavalajoshua@gmail.com>
2022-09-26 23:19:10 -0500

    Octava Tarea Realizada

--- 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import Logo from '../logoHenry.png'
-import SearchBar from './SearchBar.jsx';
-import './Nav.css';
+import React from "react";
+import Logo from "../logoHenry.png";
+import SearchBar from "./SearchBar.jsx";
+import "./Nav.css";
 
-function Nav({onSearch}) {
+function Nav({ onSearch }) {
   return (
-    <div>Nav</div>
+    <div>
+      <nav>
+        <div>
+          <img src={Logo} alt="logo" />
+          <h3>Henry - Weather App</h3>
+        </div>
+        <SearchBar onSearch={onSearch} />
+      </nav>
+    </div>
   );
-};
+}
 
 export default Nav;
