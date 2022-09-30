68e83946d04d3c5f3292ce3b975784a51bef5601
ArsazuZavala <zavalajoshua@gmail.com>
2022-09-29 23:35:44 -0500

    Decima Tarea Realizada

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export default function  Form() {
+export default function Form() {
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setInput({ ...input, [e.target.name]: e.target.value });
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+  };
+
   return (
-      <div>
-        Componente Form
-      </div>
-  )
+    <div>
+      <label>Username:</label>
+      <input
+        className={errors.username && "danger"}
+        type="text"
+        name="username"
+        value={input.username}
+        onChange={handleInputChange}
+      />
+      {errors.username && <p className="danger">{errors.username}</p>}
+      <label>Password:</label>
+      <input
+        className={errors.password && "danger"}
+        type="password"
+        name="password"
+        id="password"
+        value={input.password}
+        onChange={handleInputChange}
+      />
+      {errors.password && <p className="danger">{errors.password}</p>}
+      <input type="submit" value="Submit" />
+    </div>
+  );
+}
+
+export function validate(input) {
+  let errors = {};
+  if (!input.username) {
+    errors.username = "Username is required";
+  } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+    errors.username = "Username is invalid";
+  }
+  if (!input.password) {
+    errors.password = "Password is required";
+  } else if (!/(?=.*[0-9])/.test(input.password)) {
+    errors.password = "Password is invalid";
+  }
+
+  return errors;
 }
